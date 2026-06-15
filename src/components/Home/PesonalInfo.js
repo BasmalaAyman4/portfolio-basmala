460bfeb8250162762df68bbf5296c7d531ddc7ad
Basmala Ayman <basmalaayman650@gmail.com>
2026-06-15 17:53:28 +0300

    update portfolio

--- src/components/Home/PesonalInfo.js
+++ src/components/Home/PesonalInfo.js
@@ -3,7 +3,7 @@ import styles from '../../styles/personalinfo.module.css'
 const PesonalInfo = () => {
     const handleDownload = () => {
         
-        const fileUrl = "https://drive.google.com/file/d/14Xk-jKVLnUWD1om8cP3Sxur464efGrcz/view?usp=drive_link";
+        const fileUrl = "https://drive.google.com/file/d/1jlgMM-W3qLnyXaKa5gquF5sR-lJ2Yo47/view?usp=sharing";
         const link = document.createElement('a');
         link.href = fileUrl;
         link.download = 'Basmala-Ayman-CV.pdf'; 
